d295adcf2342317a4954a7705cf7ed337ce8de20
bubao <asd565586630@gmail.com>
2021-06-20 14:35:12 +0800

    note 更新

--- routes/note.js
+++ routes/note.js
@@ -5,9 +5,8 @@ const SqliteDB = require("../db/index");
 /**
  * 获取笔记列表
  */
-router.get("/", async function(req, res, next) {
+router.get("/", async function(req, res) {
 	const { limit = 10, offset = 0, id } = req.query;
-	console.log("xxx");
 	try {
 		const db = await SqliteDB.init();
 
@@ -22,25 +21,29 @@ router.get("/", async function(req, res, next) {
 		let result;
 
 		if (id !== undefined) {
-			result = await db.get(`SELECT * FROM note WHERE id=${id};`);
+			result = await db.get(`SELECT * FROM note WHERE id=${id} AND del_token = 0;`);
+			res.json({ id: req.query.id, data: result || {} });
 		} else {
-			const query = `SELECT * FROM note LIMIT ${limit} OFFSET ${offset}`;
+			const query = `SELECT * FROM note WHERE del_token = 0 LIMIT ${limit} OFFSET ${offset};`;
 			result = await db.all(query);
+			const total = "SELECT COUNT(*) FROM note WHERE del_token = 0";
+			res.json({ id: req.query.id, total, data: result || {} });
 		}
-		res.json({ id: req.query.id, data: result || {} });
-		console.log(result);
 	} catch (error) {
 		res.json(req.query);
 	}
 });
 
+/**
+ * 删除文章，加入到垃圾桶
+ */
 router.delete("/", async function(req, res) {
 	const { id } = req.query;
 	if (isNaN(id)) {
 		res.status(400).json({ error: "xxx" });
 		return;
 	}
-	const query = `DELETE FROM COMPANY WHERE id = ${id}`;
+	const query = `UPADATE note SET del_token = 1 WHERE id = ${id} AND del_token = 0`;
 	try {
 		const db = await SqliteDB.init();
 		const result = await db.run(query);
@@ -51,6 +54,9 @@ router.delete("/", async function(req, res) {
 	}
 });
 
+/**
+ * 更新文章
+ */
 router.put("/", async function(req, res) {
 	const { title, content, id } = req.body || {};
 	if (isNaN(id)) {
@@ -59,13 +65,16 @@ router.put("/", async function(req, res) {
 	}
 	try {
 		const db = await SqliteDB.init();
-		const result = await db.run("UPADATE note SET title = ? content = ? update_at = ? WHERE id = ?", [title, content, new Date(), id]);
+		const result = await db.run("UPADATE note SET title = ? content = ? update_at = ? WHERE id = ?", [title, content, new Date().valueOf(), id]);
 		console.log(result);
 	} catch (err) {
 		res.status(400).json({ error: "xxx" });
 	}
 });
 
+/**
+ * 新增文章
+ */
 router.post("/", async function(req, res) {
 	const { title, content } = req.body || {};
 	if (!title || !content) {
