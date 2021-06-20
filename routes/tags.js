ccd0f2fe7926447990890e499327c7a24dc9578e
bubao <asd565586630@gmail.com>
2021-06-21 00:36:34 +0800

    update tags

--- routes/tags.js
+++ routes/tags.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const SqliteDB = require("../db/index");
-// const { cherry } = require("../utils");
+const { cherry } = require("../utils");
+const Joi = require("joi");
 
 /**
  * 获取tags类型详情
@@ -16,20 +17,6 @@ router.get("/type", async (req, res) => {
 			result[name] = other;
 		});
 		res.json({ data: result });
-	} catch (error) {
-		console.log(error);
-		res.json({ error: "xxx" });
-	}
-});
-
-/**
- * 获取tags个数
- */
-router.get("/", async (req, res) => {
-	try {
-		const db = await SqliteDB.init();
-		const list = await db.all("SELECT COUNT(name) AS tatol FROM tags GROUP BY name");
-		res.json({ data: list });
 	} catch (error) {
 		console.log(error);
 		res.json({ error: "xxx" });
@@ -64,6 +51,67 @@ router.put("/type", async (req, res) => {
 		res.json({ data: list });
 	} catch (error) {
 		console.log(error);
+		res.json({ error: "xxx" });
+	}
+});
+
+/**
+ * 删除tags类型，并清除已存在的该tag的文章标记
+ */
+router.delete("/type", async (req, res) => {
+	try {
+		const { name } = req.body;
+		const db = await SqliteDB.init();
+		const query = tb => `DELETE FROM ${tb} WHERE name = ${name}`;
+		const tagsType = db.run(query("tags_type"));
+		const tags = db.run(query("tags"));
+		await Promise.all([tagsType, tags]);
+		res.json({ code: 0 });
+	} catch (error) {
+		console.log(error);
+		res.json({ error: "xxx" });
+	}
+});
+
+/**
+ * 获取tags个数
+ */
+router.get("/", async (req, res) => {
+	try {
+		const db = await SqliteDB.init();
+		const list = await db.all("SELECT name, COUNT(name) AS tatol FROM tags GROUP BY name");
+		res.json({ data: list });
+	} catch (error) {
+		console.log(error);
+		res.json({ error: "xxx" });
+	}
+});
+/**
+ * 文章新增tags
+ */
+router.post("/", async (req, res) => {
+	try {
+		const db = await SqliteDB.init();
+		const time = new Date().valueOf();
+		const validate = await Joi.object({
+			name: Joi.string().alphanum().min(1).required(),
+			note_id: Joi.number().required()
+		}).validateAsync(req.body).catch(error => {
+			return { error };
+		});
+		if (validate.error) {
+			return res.status.json(validate.error);
+		}
+		const [{ name }, { note_id }] = await Promise.all([
+			db.get(`SELECT name FROM tags_type WHERE name = ${req.body.name}`),
+			db.get(`SELECT id AS note_id FROM note WHERE name = ${req.body.note_id}`)
+		]);
+		if (name === undefined || note_id === undefined) {
+			return res.json({ error: "xxx" });
+		}
+		const id = cherry.id();
+		db.run("INSERT INTO tags SET (id,name,note_id,created_at,updated_at) VALUES (?,?,?,?,?)", [id, name, note_id, time, time]);
+	} catch (error) {
 		res.json({ error: "xxx" });
 	}
 });
