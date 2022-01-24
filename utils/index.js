babb3cfb7f7926456065af6a5d0a4532a8a303e5
bubao <asd565586630@gmail.com>
2022-01-24 13:48:57 +0800

    添加token验证方法

--- utils/index.js
+++ utils/index.js
@@ -3,15 +3,17 @@
  * @author: bubao
  * @Date: 2021-06-21 08:34:12
  * @LastEditors: bubao
- * @LastEditTime: 2022-01-23 17:13:40
+ * @LastEditTime: 2022-01-24 13:47:55
  */
 const GenId = require("./cheery");
 const errcode = require("./errcode");
 const md5Slat = require("./md5Slat");
-const token = require("./token");
+const { generateToken, verifyToken } = require("./token");
+
 module.exports = {
 	cherry: GenId.init(),
 	errcode,
 	md5Slat,
-	generateToken: token
+	generateToken,
+	verifyToken
 };
--- utils/token.js
+++ utils/token.js
@@ -3,13 +3,35 @@
  * @author: bubao
  * @Date: 2022-01-23 16:59:02
  * @LastEditors: bubao
- * @LastEditTime: 2022-01-23 17:39:38
+ * @LastEditTime: 2022-01-24 13:47:30
  */
 const jwt = require("jsonwebtoken");
+const Redis = require("../src/db/redis");
 function generateToken(paylod, expiresIn = 60 * 60 * 2) {
 	return jwt.sign(paylod, "token", {
 		expiresIn
 	});
 }
+/**
+ * @description 验证token
+ * @author bubao
+ * @date 2022-01-24 13:01:06
+ * @param {*} token
+ * @param {number} [type=1] 1: accessToken 2:refreshToken
+ */
+async function verifyToken(token, type = 1) {
+	let res;
+	try {
+		res = jwt.verify(token, "token");
+	} catch (error) {
+		throw new Error(42000);
+	}
+	const redis = Redis.init();
+	const redisToken = await redis.get(`${JSON.parse(res).id}#${type === 1 ? "access_token" : "refresh_token"}`);
+	if (redisToken !== token) {
+		throw new Error(42001);
+	}
+	return JSON.parse(res);
+}
 
-module.exports = generateToken;
+module.exports = { generateToken, verifyToken };
