daf9ec54a308aa2b70af66cc84701d7023f86f8f
bubao <asd565586630@gmail.com>
2022-02-21 00:16:08 +0800

    transaction

--- src/routes/v1/register.js
+++ src/routes/v1/register.js
@@ -3,7 +3,7 @@
  * @author: bubao
  * @Date: 2022-01-25 21:49:49
  * @LastEditors: bubao
- * @LastEditTime: 2022-02-20 15:51:04
+ * @LastEditTime: 2022-02-20 18:20:11
  */
 const express = require("express");
 const router = express.Router();
@@ -13,19 +13,19 @@ const prisma = require("../../db/notion.prisma").init();
 const redis = require("../../db/redis").init();
 
 // info 通用方法
-const { errcode, MyError, md5Slat } = require("../../../utils/index");
+const { errcode, MyError, md5Slat, cherry } = require("../../../utils/index");
 const { register } = require("../../joi/v1/register.joi");
 /**
- * register 注册
+ * register 邮箱密码注册
  */
 router.post("", async function(req, res, next) {
 	try {
 		// info 检验参数
 		// info 加锁
-		await register.validateAsync(req.body).catch(err => {
+		const { name, email, password } = await register.validateAsync(req.body).catch(err => {
 			throw new MyError(40001, err);
 		});
-		const redisLock = await redis.set(`register#${req.body.email}`, true, "Ex", 5, "Nx");
+		const redisLock = await redis.set(`register#${email}`, true, "Ex", 5, "Nx");
 		if (!redisLock) {
 			// 加锁失败
 			throw new MyError(40004);
@@ -36,17 +36,33 @@ router.post("", async function(req, res, next) {
 			}
 		});
 		if (user) {
-			await redis.del(`register#${req.body.email}`);
+			await redis.del(`register#${email}`);
 			throw new MyError(41003);
 		}
+		const meta_user_id = cherry.id();
+		const meta_user_role = "editor";
 		// info 创建用户
-		await prisma.users.create({
-			name: req.body.name,
-			email: req.body.email,
-			password: await md5Slat(req.body.password)
-		});
+		await prisma.$transaction([
+			prisma.notionUser.create({
+				data: {
+					id: meta_user_id,
+					name: name,
+					email: email,
+					password: await md5Slat(password),
+					meta_user_id,
+					meta_user_role
+				}
+			}),
+			prisma.userRoot.create({
+				data: {
+					id: meta_user_id,
+					meta_user_id,
+					meta_user_role
+				}
+			})
+		]);
 		// info 删除锁
-		await redis.del(`register#${req.body.email}`);
+		await redis.del(`register#${email}`);
 		const { status, body } = errcode(0);
 		res.status(status).send(body);
 	} catch (error) {
